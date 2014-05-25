9d17206c26aafce9a1c78cf6c0a8703c28a44a8b
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-05-26 01:03:17 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nima::Application.config.secret_key_base = '31e702137ef3d31dd05c658b4ec77579dd6f54cf4e1776790159eea6b0986c2b53a26d851add83d432c23793d72881bba91baf62340a4ca01be4b4b2101c3b70'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Nima::Application.config.session_store :cookie_store, key: '_nima_session'
