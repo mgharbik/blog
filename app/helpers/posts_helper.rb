ee7ed7d4a2bf03b2f256d6f79dcac7119b058fa7
Gharbi Mohammed <simo163@hotmail.co.uk>
2014-05-26 04:05:45 +0200

    Refactoring post index view

---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,20 @@
+module PostsHelper
+
+	def posts_listing_items(posts)
+		content_tag(:div, :class => 'col-sm-8 blog-main') do
+	    	render posts
+		end
+	end
+
+	def post_title_item(post)
+		content_tag(:h2, :class => 'blog-post-title') do
+	    	post.title.html_safe
+		end
+	end
+
+	def post_date_item(post)
+		content_tag(:p, :class => 'blog-post-meta') do
+	    	post.created_at.to_s
+		end
+	end
+end
